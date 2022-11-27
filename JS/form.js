a4918b30d4159151e845ec87111f669d6fcb3514
BenjiWarburton <09bwarburton@brightoncollege.net>
2022-11-27 20:14:02 +0000

    js emailer

--- JS/form.js
+++ JS/form.js
@@ -1,60 +1,20 @@
-// show a message with a type of the input
-function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
+function validateForm() {
+  let name = document.forms["myForm"]["fname"].value;
+  let email = document.forms["myForm"]["email"].value;
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (email == "") {
+	  alert("email must be filled out")
+	  return false;
+  }
+
+  let y = document.getElementById("myForm");
+    let z = document.getElementById("myForm").getAttribute("action")
+	let newatt = z + "?subject="+name;
+    alert(newatt);
+    y.setAttribute.action = newatt;
+    window.open(newatt);
+    
 }
-
-function showError(input, message) {
-	return showMessage(input, message, false);
-}
-
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
-
-function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
-}
-
-function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
-}
-
-const form = document.querySelector("#signup");
-
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
-
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
-
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-	// if valid, submit the form.
-	let action;
-	if (nameValid && emailValid) {
-		action = "mailto:jake.bowie1@gmail.com" //This is the web browser to send an email to this address. need to add a subject of their name, body of their comments and from their email
-	}
-});
