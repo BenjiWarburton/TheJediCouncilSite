6db58a54d440e74d2f465a24cdeeff4036c4e3e6
BenjiWarburton <09bwarburton@brightoncollege.net>
2022-11-28 12:51:23 +0000

    js emailer now with email body

--- JS/form.js
+++ JS/form.js
@@ -1,6 +1,7 @@
 function validateForm() {
   let name = document.forms["myForm"]["fname"].value;
   let email = document.forms["myForm"]["email"].value;
+  let comments = document.forms["myForm"]["comments"].value;
   if (name == "") {
     alert("Name must be filled out");
     return false;
@@ -12,7 +13,7 @@ function validateForm() {
 
   let y = document.getElementById("myForm");
     let z = document.getElementById("myForm").getAttribute("action")
-	let newatt = z + "?subject="+name;
+	let newatt = z + "?subject="+name+"&body="+comments;
     alert(newatt);
     y.setAttribute.action = newatt;
     window.open(newatt);
